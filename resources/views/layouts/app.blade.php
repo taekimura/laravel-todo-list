f216d5b1fdc76cdcafe51c62967a324f7e08c501
Tae Kimura <sheepman7893@gmail.com>
2024-03-18 12:29:21 +0900

    fix: add index and layout

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>Laravel To-do List App</title>
+</head>
+
+<body>
+  <h1>@yield('title')</h1>
+  <div>
+    @yield('content')
+  </div>
+</body>
+
+</html>

e0f9edec42e7650a01eefe23f3f1e15bdf336d71
Tae Kimura <sheepman7893@gmail.com>
2024-03-24 17:58:07 +0900

    refactor: route model binding form request, mass assignment

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -8,6 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     use HasFactory;
+    protected $fillable = ['title', 'description', 'long_description'];
+    // protected $guarded = ['secret'];
 }
 
 // php artisan make:model Task -m
